d90d4e774b6695e985f1ae0d25e1e6467cc243e2
Mark Elston <Mark.Elston@advantest.com>
2019-05-16 15:54:29 -0700

    Added additional step for part 5.

---
+++ src/main/kotlin/kfizzbuzz5a.kt
@@ -0,0 +1,24 @@
+package com.elsoft.kfizzbuzz5a
+
+import com.elsoft.extensions.toStringVal
+import com.elsoft.utilities.genFBFun
+import com.elsoft.utilities.then
+
+fun main(args: Array<String>) {
+
+    val fizz3 = genFBFun(3, "fizz")
+    val buzz5 = genFBFun(5, "buzz")
+    val bang7 = genFBFun(7, "bang")
+
+    val toPair : (Int) -> Pair<Int, String> = { Pair(it, "") }
+    val extractStr : (Pair<Int, String>) -> String  = { it.toStringVal() }
+
+    // 'then' is an extension function on function types useful for composition.
+    // It is defined in the project's utilities package.
+    val doFB =
+            toPair then fizz3 then buzz5 then bang7 then extractStr
+
+    1.rangeTo(105)
+        .map(doFB)
+        .forEach { println(it) }  // Overloads prevent using function reference
+}
--- src/main/kotlin/kfizzbuzz5b.kt
+++ src/main/kotlin/kfizzbuzz5b.kt
@@ -1,4 +1,4 @@
-package com.elsoft.kfizzbuzz5
+package com.elsoft.kfizzbuzz5b
 
 import com.elsoft.extensions.toStringVal
 import com.elsoft.utilities.genFBFun
@@ -6,6 +6,14 @@ import com.elsoft.utilities.then
 
 fun main(args: Array<String>) {
 
+    val doFB = createFB()
+
+    1.rangeTo(105)
+        .map(doFB)
+        .forEach { println(it) }  // Overloads prevent using function reference
+}
+
+fun createFB() : (Int) -> String {
     val fizz3 = genFBFun(3, "fizz")
     val buzz5 = genFBFun(5, "buzz")
     val bang7 = genFBFun(7, "bang")
@@ -13,12 +21,7 @@ fun main(args: Array<String>) {
     val toPair : (Int) -> Pair<Int, String> = { Pair(it, "") }
     val extractStr : (Pair<Int, String>) -> String  = { it.toStringVal() }
 
-    // 'then' is an extension function on function types useful for composition.
-    // It is defined in the project's utilities package.
     val doFB =
             toPair then fizz3 then buzz5 then bang7 then extractStr
-
-    1.rangeTo(105)
-        .map(doFB)
-        .forEach { println(it) }  // Overloads prevent using function reference
+    return doFB
 }
